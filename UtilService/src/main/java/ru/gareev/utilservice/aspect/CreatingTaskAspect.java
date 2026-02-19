980ebe113ded7106060c1180895e64e465e8f1de
Mark Gareev <margarin2000@mail.ru>
2026-02-19 05:29:45 +0300

    Add creation logic in utils, add logging CREATE task

---
+++ UtilService/src/main/java/ru/gareev/utilservice/aspect/CreatingTaskAspect.java
@@ -0,0 +1,22 @@
+package ru.gareev.utilservice.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class CreatingTaskAspect {
+    @Around("@annotation(CreatingTask)")
+    public Object addLogTag(ProceedingJoinPoint joinPoint) throws Throwable {
+        MDC.put("TASK", "CREATE");
+        try {
+            return joinPoint.proceed();
+        } finally {
+            MDC.remove("TASK");
+        }
+
+    }
+}
--- UtilService/src/main/java/ru/gareev/utilservice/service/DocumentServiceImpl.java
+++ UtilService/src/main/java/ru/gareev/utilservice/service/DocumentServiceImpl.java
@@ -1,31 +1,41 @@
 package ru.gareev.utilservice.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ru.gareev.utilservice.api.dto.DocumentCreateRequest;
+import ru.gareev.utilservice.aspect.CreatingTask;
 
 @Service
 @RequiredArgsConstructor
-public class DocumentServiceImpl implements DocumentService{
+@Slf4j
+public class DocumentServiceImpl implements DocumentService {
     private final DocumentConnectionProvider provider;
+
     @Override
     public boolean ping() {
         return provider.ping();
     }
 
     @Override
+    @CreatingTask
     public void createDocuments(Long documentsCount) {
         DocumentCreateRequest request = DocumentCreateRequest.builder()
                 .author("utilService")
                 .build();
-        for(long l = 0; l < documentsCount; l++){
+        log.info("start creating {} documents",documentsCount);
+        long createdCount = 0L;
+        for (long l = 0; l < documentsCount; l++) {
             request.setName("cycle creating " + l);
             ResponseEntity<Void> response = provider.create(request);
-            if(!response.getStatusCode().is2xxSuccessful()){
-                //log error when adding logs
-                System.out.println("error creating document");
+            if (!response.getStatusCode().is2xxSuccessful()) {
+                log.info("get error while creating document http code {}", response.getStatusCode().value());
+            }else{
+                log.info("successfully create document");
+                createdCount++;
             }
+            log.info("created {} documents",createdCount);
         }
     }
 }
